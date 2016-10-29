e2552aa2209d8bae0be7968603e4a4b11f43a99a
chwilfing <christoph@wilfing.biz>
2016-10-29 13:40:45 +0200

    WebHook Change

--- Meetup 2016-11-17/DeploymentSample/TriggerWebhook.ps1
+++ Meetup 2016-11-17/DeploymentSample/TriggerWebhook.ps1
@@ -1,13 +1,13 @@
 ﻿<#
 
 #>
-$WebHookUri = 'https://s9events.azure-automation.net/webhooks?token=DGGfgc8BcXAOtkXrpwK%2f6Q%2brT2EwmronyK%2fRkMjBwzQ%3d'
+$WebHookUri = 'https://s9events.azure-automation.net/webhooks?token=dxZ%2f%2fkGTvMnTCVzrFBhJuRXNSGJrfSyLn3j8gEt%2fSN0%3d' # newServer
+#$WebHookUri = 'https://s9events.azure-automation.net/webhooks?token=DGGfgc8BcXAOtkXrpwK%2f6Q%2brT2EwmronyK%2fRkMjBwzQ%3d' #Importtemplate
 
 $headers = @{'From'='Christoph@wilfing.biz';
              'Date'=$(Get-Date)}
 
 $Parameter  = @{ NumberOfInstances = 2;
-                 ServerName        = 'VMMeetUP1';
                  RGName            = 'CW-DSC-Test01';
                  Template          = 'https://raw.githubusercontent.com/chwilfing/meetupazurewien/master/Meetup%202016-11-17/DeploymentSample/WindowsServer.json';
                  TemplateInput     = 'https://raw.githubusercontent.com/chwilfing/meetupazurewien/master/Meetup%202016-11-17/DeploymentSample/WindowsServer.parameter.json';
