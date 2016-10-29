073e48c7a1a925a5e79988701c980b4d6751e763
chwilfing <christoph@wilfing.biz>
2016-10-29 13:33:38 +0200

    UPdate Parameter

--- Meetup 2016-11-17/DeploymentSample/TriggerWebhook.ps1
+++ Meetup 2016-11-17/DeploymentSample/TriggerWebhook.ps1
@@ -1,13 +1,21 @@
-﻿
+﻿<#
 
+#>
+$WebHookUri = 'https://s9events.azure-automation.net/webhooks?token=DGGfgc8BcXAOtkXrpwK%2f6Q%2brT2EwmronyK%2fRkMjBwzQ%3d'
 
-$uri = 'https://s9events.azure-automation.net/webhooks?token=DGGfgc8BcXAOtkXrpwK%2f6Q%2brT2EwmronyK%2fRkMjBwzQ%3d'
-$headers = @{"From"="Christoph@wilfing.biz";"Date"="05/28/2015 15:47:00"}
+$headers = @{'From'='Christoph@wilfing.biz';
+             'Date'=$(Get-Date)}
 
-$Parameter  = @(
-                @{ NumberOfInstances=2}
-              )
+$Parameter  = @{ NumberOfInstances = 2;
+                 ServerName        = 'VMMeetUP1';
+                 RGName            = 'CW-DSC-Test01';
+                 Template          = 'https://raw.githubusercontent.com/chwilfing/meetupazurewien/master/Meetup%202016-11-17/DeploymentSample/WindowsServer.json';
+                 TemplateInput     = 'https://raw.githubusercontent.com/chwilfing/meetupazurewien/master/Meetup%202016-11-17/DeploymentSample/WindowsServer.parameter.json';
+                 AZConnectionName  = 'AzureRunAsConnection';
+                 }
+
 $body = ConvertTo-Json -InputObject $Parameter
 
-$response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+$response = Invoke-RestMethod -Method Post -Uri $WebHookUri -Headers $headers -Body $body
 
+#Get-AzureRmResourceGroupDeploymentOperation -DeploymentName $response.JobIds.ToString() -ResourceGroupName $Parameter.RGName
